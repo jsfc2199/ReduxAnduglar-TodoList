8994c01a0550f41642a45be412ff1700dc9e18b3
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-13 18:37:27 -0500

    Se añaden las dev tools para redux y se define el principio del AppState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +7,11 @@ import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, TodoModule, StoreModule.forRoot({todos: todoReducer})],
+  imports: [BrowserModule, AppRoutingModule, TodoModule, StoreModule.forRoot({todos: todoReducer}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })],
   providers: [],
   bootstrap: [AppComponent],
 })

bf2c0d13995c051491f3e965d97101d2c03bc9b7
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 15:47:50 -0500

    cambio del estado del filtro implementado

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import* as actions from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
   styleUrls: ['./todo-footer.component.css'],
 })
-export class TodoFooterComponent {}
+export class TodoFooterComponent implements OnInit{
+
+  filtroActual: actions.filtrosValidos = "todos"
+
+  filtros: actions.filtrosValidos[] = ['todos','completados','pendientes']
+
+  constructor(private store:Store<AppState>){}
+  ngOnInit(): void {
+    this.store.select('filtro').subscribe(filtro => {
+      this.filtroActual = filtro
+
+    })
+  }
+
+  cambiarFiltro(filtro: actions.filtrosValidos){
+    this.store.dispatch(actions.setFiltro({filtro: filtro}))
+  }
+}
